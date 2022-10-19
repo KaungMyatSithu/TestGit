e6911bc9036f03309ecbc658e2d81c4549d1a48a
KaungMyatSithu <myat64962@gmail.com>
2022-10-19 19:12:10 +0630

    Text SSH Success

--- src/TextGit/main.java
+++ src/TextGit/main.java
@@ -6,6 +6,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
 		System.out.println("Added PrintStream 1");
+		System.out.println("Change new Style");
 	}
 
 }
